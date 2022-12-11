e32e1862a5c2aefbde65f1157373f9076493e20f
MATT-07 <117632549+MATT-07@users.noreply.github.com>
2022-12-11 17:01:08 +0100

    Update dmcassebirque.py

--- dmcassebirque.py
+++ dmcassebirque.py
@@ -21,7 +21,7 @@ briques = []
 forceVertical = 1
 forceHorizontal = 0
 
-vie_joueur
+vie_joueur = 20
 
 def plateforme_deplacement(x):
     """déplacement avec les touches de directions"""
@@ -33,63 +33,56 @@ def plateforme_deplacement(x):
             x = x - 1
     return x
 
+
 def balle_deplacement(x, y, forceVertical, forceHorizontal, vie_joueur):
     """ fonction qui s'occupe des deplacements
     de la balle qui va vers le bas et les collision avec la plateforme"""
-    y = y + forceVertical *1.07
+    y = y + forceVertical *1.5
     x = x + forceHorizontal
     if y >= 127:
         y = 70
         x = 60
         forceHorizontal = 0
         vie_joueur -= 1
-    return x,y,forceHorizontal,vie_joueur
+    return x, y, forceHorizontal, vie_joueur
+
 
 def creation_brique(briques):
-    """
-    affichage successif des briques les unes à la suite des autres avec comme paramètres de position i et k.
-    """
+    """affichage successif des briques les unes à la suite des autres avec comme paramètres de position i et k."""
     for y in range(5, 50, 5):
         for x in range(10, 120, 10):
             v = random.randint(1, 3)
-            briques.append((x,y,v))
+            briques.append((x, y, v))
     return briques
 
-def collision(balle_x, balle_y, forceVertical, forceHorizontal, briques):
-    """
-	fonction qui gère les collisions entre la balle et les briques
-    """
+
+def collision(balle_x, balle_y, forceVertical, briques, forceHorizontal):
+    """fonction qui gère les collisions entre la balle et les briques"""
     for b in briques:
-    	vie = b[2]
-    	# si la balle tape en dessous de la brique
-    	if balle_x <= b[0] + 8 and balle_y <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y + 2 >= b[1] + 4:
-        	forceVertical = 1
-        	forceHorizontal = 0
-        	vie -= 1
-        	break
+        # si la balle tape en dessous de la brique
+        if balle_x <= b[0] + 8 and balle_y <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y + 2 >= b[1] + 4:
+            forceVertical = 1
+	    briques.remove(b)
+            break
         # si la balle arrive sur le coté gauche
-		elif balle_x + 2 <= b[0] + 3 and balle_y <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y + 2 >= b[1]:
-        	forceVertical = 1
-        	forceHorizontal = -1
-        	vie -= 1
-        	break
+        elif balle_x <= b[0] + 3 and balle_y <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y >= b[1]:
+            forceVertical = 1
+            forceHorizontal = -1
+	    briques.remove(b)
+            break
         # si la balle arrive à droite
         elif balle_x + 2 <= b[0] + 8 and balle_y <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y + 2 >= b[1]:
-        	forceVertical = 1
-        	forceHorizontal = 1
-        	vie -= 1
-        	break
-        # si la balle arrive au dessus
-        elif balle_x + 2 <= b[0] + 8 and balle_y + 2 <= b[1] and balle_x + 2 >= b[0] and balle_y + 2 >= b[1]:
             forceVertical = 1
-            forceHorizontal = 0
-            vie -= 1
-            break   
-	if vie == 0:
-		briques.remove(b)     
+            forceHorizontal = 1
+            briques.remove(b)
+            break
+        # si la balle arrive au dessus
+        elif balle_x + 2 <= b[0] + 8 and balle_y + 2 <= b[1] + 2 and balle_x + 2 >= b[0] and balle_y + 2 >= b[1] + 4:
+            forceVertical = -1
+            briques.remove(b)
+            break
 
-    return forceVertical, forceHorizontal, briques
-
+    return forceVertical, briques, forceHorizontal
 
 # =========================================================
 # == UPDATE
@@ -97,27 +90,27 @@ def collision(balle_x, balle_y, forceVertical, forceHorizontal, briques):
 def update():
     """mise à jour des variables (30 fois par seconde)"""
 
-    global plateforme_x, plateforme_y, balle_x, balle_y, forceVertical, forceHorizontal, briques
+    global plateforme_x, plateforme_y, balle_x, balle_y, forceVertical, forceHorizontal, briques, vie_joueur, vie
 
     # mise à jour de la position du plateforme
     plateforme_x = plateforme_deplacement(plateforme_x)
-    
+
     # deplacemebt de la balle
-    balle_x, balle_y, forceHorizontal, vie_joueur = balle_deplacement(balle_x, balle_y, forceVertical, forceHorizontal,vie_joueur)
+    balle_x, balle_y, forceHorizontal, vie_joueur = balle_deplacement(balle_x, balle_y, forceVertical, forceHorizontal,
+                                                                      vie_joueur)
 
-    #collision entre la balle et les briques
-    forceVertical, forceHorizontal, briques = collision(balle_x, balle_y, forceVertical, forceHorizontal, briques)
+    # collision entre la balle et les briques
+    forceVertical, briques, forceHorizontal = collision(balle_x, balle_y, forceVertical, briques, forceHorizontal)
 
-    #condition lorsque la balle touche la plateforme
-    if plateforme_x + 20 >= balle_x + 2 >= plateforme_x and balle_y + 2 <= plateforme_y + 5 and balle_y + 2 >= plateforme_y :
+    # condition lorsque la balle touche la plateforme
+    if plateforme_x + 20 >= balle_x + 2 >= plateforme_x and balle_y + 2 <= plateforme_y + 5 and balle_y + 2 >= plateforme_y:
         forceVertical = -1
-        forceHorizontal = 0
-    elif balle_x + 2 <= plateforme_x + 31 and balle_y + 2 <= plateforme_y + 5 and balle_x + 2 >= plateforme_x + 21 and balle_y + 2 >= plateforme_y :
-        forceVertical = -1
-        forceHorizontal = -1
-    elif balle_x <= plateforme_x -1 and balle_y <= plateforme_y + 5 and balle_x >= plateforme_x - 10 and balle_y >= plateforme_y:
+    elif balle_x + 2 <= plateforme_x + 31 and balle_y + 2 <= plateforme_y + 5 and balle_x + 2 >= plateforme_x + 21 and balle_y + 2 >= plateforme_y:
         forceVertical = -1
         forceHorizontal = 1
+    elif balle_x <= plateforme_x - 1 and balle_y <= plateforme_y + 5 and balle_x >= plateforme_x - 10 and balle_y >= plateforme_y:
+        forceVertical = -1
+        forceHorizontal = -1
 
     elif balle_y < 1:
         forceVertical = 1
@@ -135,20 +128,27 @@ def draw():
 
     # vide la fenetre
     pyxel.cls(6)
-	if vie_joueur != 0:
-    	# forme de la platform
-    	pyxel.rect(plateforme_x, plateforme_y, 20, 2, 3)
-    	pyxel.tri(plateforme_x, plateforme_y+4, plateforme_x-10, plateforme_y+4, plateforme_x, plateforme_y, 2)
-    	pyxel.tri(plateforme_x+20, plateforme_y+4, plateforme_x+30, plateforme_y+4, plateforme_x+20, plateforme_y, 2)
+    if vie_joueur != 0:
+        # forme de la platform
+        pyxel.rect(plateforme_x, plateforme_y, 20, 2, 4)
+        pyxel.tri(plateforme_x, plateforme_y + 4, plateforme_x - 10, plateforme_y + 4, plateforme_x, plateforme_y, 4)
+        pyxel.tri(plateforme_x + 20, plateforme_y + 4, plateforme_x + 30, plateforme_y + 4, plateforme_x + 20,
+                  plateforme_y, 4)
 
-    	# dessin de la balle
-    	pyxel.circb(balle_x, balle_y, 2, 5)
+        # dessin de la balle
+        pyxel.circb(balle_x, balle_y, 2, 5)
 
-    	# dessin des briques
-    	for i in briques:
-    	    pyxel.rect(i[0], i[1], 8, 2, i[2])
-	else:
-		pyxel.text("game over")
+        # dessin des briques
+        for i in briques:
+            pyxel.rect(i[0], i[1], 8, 2, i[2]+6)
+        if not briques:
+            pyxel.cls(0)
+            pyxel.text(50, 50, "bien jouer :)", 6)
+            pyxel.show()
+    else:
+        pyxel.cls(0)
+        pyxel.text(50, 50, "game over :(", 6)
+        pyxel.show()
 
 # creation des briques
 briques = creation_brique(briques)
