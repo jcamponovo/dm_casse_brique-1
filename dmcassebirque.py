f752f1b8fa9f05e05ab109a341f702ba79393a35
jcamponovo <50249274+jcamponovo@users.noreply.github.com>
2023-01-15 22:40:53 +0100

    Update dmcassebirque.py

--- dmcassebirque.py
+++ dmcassebirque.py
@@ -1,14 +1,4 @@
-import pyxel, random
 
-# pip install -U pyxel
-
-# taille de la fenetre 128x128 pixels
-# ne pas modifier
-pyxel.init(128, 128, title="Nuit du c0de")
-
-# position initiale du plateforme
-plateforme_x = 50
-plateforme_y = 100
 
 import pyxel, random
 
